9b6991d29d15cd6a97fd4bea7738a148c991d463
NikolaChuchukov <nikola.chuchukov@gmail.com>
2018-07-19 12:26:50 +0300

    da

--- src/test/java/selenium.java
+++ src/test/java/selenium.java
@@ -1,16 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class selenium {
 
-
-    public static void main(String[] args) {
+@Test
+    public void fantasy() {
         WebDriver driver = null;
 
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.navigate().to("https://fantasy.premierleague.com/");
         driver.close();
+
+        System.out.println("Successfully opened fantasy football");
 }
 
 }
